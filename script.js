c4ff814ea3b12e97e91218aa07674d99db57a3ad
RAHUL THAKUR <76059700+rahulthakur19@users.noreply.github.com>
2022-04-29 19:56:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
-// var list1 = [];
-// 		var list2 = [];
-// 		var list3 = [];
-//         var list4 =[];
+
      var a,b,c,d,e;
 		var n = 1;
 		var x = 0;
@@ -29,23 +26,4 @@
 			x++;
         }
 
-		// function AddRow(){
-
-		// 	var AddRown = document.getElementById('show');
-		// 	var NewRow = AddRown.insertRow(n);
-
-		// 	list1[x] = document.getElementById("fname").value;
-		// 	list2[x] = document.getElementById("email").value;
-		// 	list3[x] = document.getElementById("dob").value;
-
-		// 	var cel1 = NewRow.insertCell(0);
-		// 	var cel2 = NewRow.insertCell(1);
-		// 	var cel3 = NewRow.insertCell(2);
-
-		// 	cel1.innerHTML = list1[x];
-		// 	cel2.innerHTML = list2[x];
-		// 	cel3.innerHTML = list3[x];
-
-		// 	n++;
-		// 	x++;
-		// }
+		
